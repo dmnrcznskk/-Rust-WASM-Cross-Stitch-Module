1b2d4f4a18a78c62484acd347d045f15963587d9
dmnrcznsk <dmnrcznsk@gmail.com>
2026-09-14 17:03:46 +0200

    Split previous solution to separete functions

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,48 @@
 use image::{ImageReader, RgbaImage};
 use image::imageops;
 use imagequant::RGBA;
+use rgb::FromSlice;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let img = ImageReader::open("image.png")?.decode()?;
+fn load_and_resize_img(name: &str, width: u32, height: u32) -> Result<RgbaImage, Box<dyn std::error::Error>> {
+    let img = ImageReader::open(name)?.decode()?;
+    let resized = imageops::resize(&img, width, height, imageops::FilterType::Lanczos3);
+    Ok(resized)
+}
 
-    let resized = imageops::resize(&img, 250, 250, imageops::FilterType::Lanczos3);
-    let width = resized.width() as usize;
-    let height = resized.height() as usize;
+fn quantize_image(img: &RgbaImage, num_colors: u8) -> Result<RgbaImage, Box<dyn std::error::Error>> {
+    let width = img.width();
+    let height = img.height();
 
-    let pixels: Vec<RGBA> = resized
-        .pixels()
-        .map(|p| RGBA::new(p[0], p[1], p[2], p[3]))
-        .collect();
+    let pixels: Vec<RGBA> = img.as_raw().as_rgba().to_vec();
 
     let mut liq = imagequant::new();
-    liq.set_max_colors(16)?;
+    liq.set_max_colors(num_colors as u32)?;
 
-    let mut liq_img = liq.new_image(pixels, width, height, 0.0)?;
+    let mut liq_img = liq.new_image(pixels, width as usize, height as usize, 0.0)?;
 
     let mut res = liq.quantize(&mut liq_img)?;
-    res.set_dithering_level(1.0)?; 
+    res.set_dithering_level(1.0)?;
 
     let (palette, indices) = res.remapped(&mut liq_img)?;
 
-    let mut out_buf = Vec::with_capacity(width * height * 4);
-    for idx in indices {
-        let c = palette[idx as usize];
-        out_buf.extend_from_slice(&[c.r, c.g, c.b, c.a]);
-    }
+    let raw_buf: Vec<u8> = indices
+        .iter()
+        .flat_map(|&idx| {
+            let c = palette[idx as usize];
+            [c.r, c.g, c.b, c.a]
+        })
+        .collect();
 
-    let out_img = RgbaImage::from_raw(width as u32, height as u32, out_buf)
-        .ok_or("nieprawidłowy rozmiar bufora")?;
-    out_img.save("processed.png")?;
+    RgbaImage::from_raw(width, height, raw_buf)
+        .ok_or_else(|| "Wrong buffer size".into())
+}
 
-    println!("{} x {}", width, height);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resized = load_and_resize_img("image.png", 250, 250)?;
+    let quantized = quantize_image(&resized, 16)?;
+
+    quantized.save("processed.png")?;
+
+    println!("{} x {}", quantized.width(), quantized.height());
     Ok(())
 }
