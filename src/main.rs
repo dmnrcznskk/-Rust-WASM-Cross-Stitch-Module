00b52b8e9e46df40b72bdd4b685a9faeab799382
dmnrcznskk <dmnrcznsk@gmail.com>
2026-09-16 22:22:53 +0200

    Mapping quantized image colors to dmc palette

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use image::{ImageReader, RgbaImage};
+use image::{ImageReader, RgbaImage, ImageBuffer};
 use image::imageops;
 use imagequant::RGBA;
 use rgb::FromSlice;
+use quantize_palette::palette::{Metric, Palette};
+use quantize_palette::quantize::{quantize, AlphaMode, Dither};
+
 
 fn load_and_resize_img(name: &str, width: u32, height: u32) -> Result<RgbaImage, Box<dyn std::error::Error>> {
     let img = ImageReader::open(name)?.decode()?;
@@ -37,15 +40,62 @@ fn quantize_image(img: &RgbaImage, num_colors: u8) -> Result<RgbaImage, Box<dyn 
         .ok_or_else(|| "Wrong buffer size".into())
 }
 
+#[derive(Debug)]
+struct DmcColor {
+    dmc_code: String,
+    color_name: String,
+    rgb: [u8; 3],
+}
+
+fn colors_csv_to_vec(path: &str) -> Result<Vec<DmcColor>, Box<dyn std::error::Error>> {
+    let mut colors = Vec::new();
+    let mut rdr = csv::Reader::from_path(path)?;
+    for result in rdr.records() {
+        let record = result?;
+        let dmc_code = record[0].trim().to_string();
+        let color_name = record[1].trim().to_string();
+        let r: u8 = record[3].trim().parse()?;
+        let g: u8 = record[4].trim().parse()?;
+        let b: u8 = record[5].trim().parse()?;
+
+        let color = DmcColor { dmc_code, color_name, rgb: [r, g, b]};
+        colors.push(color);
+    }
+
+    Ok(colors)
+}
+
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let resized = load_and_resize_img("image.png", 250, 250)?;
-    // let quantized = quantize_image(&resized, 32)?;
+    let args: Vec<String> = std::env::args().collect();
+    let img_name = &args[1];
 
-    // quantized.save("processed.png")?;
+    let full_path = format!("test_images/{img_name}");
 
-    // println!("{} x {}", quantized.width(), quantized.height());
+    let resized = load_and_resize_img(&full_path, 250, 250)?;
+    let quantized = quantize_image(&resized, 32)?;
 
-    colors_csv_to_vec();
+    quantized.save("processed.png")?;
+
+    println!("{} x {}", quantized.width(), quantized.height());
+
+    let colors = colors_csv_to_vec("dmc_palette.csv")?;
+
+    let mut for_palette = Vec::new();
+    for color in &colors {
+        let rgb = color.rgb;
+        for_palette.push(rgb);
+    }
+
+
+    let palette = Palette::from_colors(for_palette);
+
+    let out = quantize(&quantized, &palette, Metric::Oklab, AlphaMode::Binarize(128), Dither::None);
+
+
+
+    out.save("processed_with_palette.png")?;
 
     Ok(())
 }
